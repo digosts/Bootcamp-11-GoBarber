e7db31db14739d31575a4cbfd9cc35f7c4ce19a3
digosts <rsantosdesenv@gmail.com>
2020-06-16 16:03:22 -0300

    Commit ajustes e novas implementções.

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'rsantosdesenv@gmail.com',
+      name: 'Rodrigo Santos',
+    },
+  },
+} as IMailConfig;
